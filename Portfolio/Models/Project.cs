4d8bfbc77a2f2b661dcc022f2f51ae20d2589761
Marcelino <Marcelino@LTP-CCTV-3>
2025-08-08 16:45:22 -0400

    clase de proyecto, y ajustes a textos

---
+++ Portfolio/Models/Project.cs
@@ -0,0 +1,12 @@
+﻿namespace Portfolio.Models
+{
+    public class Project
+    {
+        public required string Title { get; set; }
+        public string FrontImage { get; set; } =null!;
+        public string Description { get; set; } = null!;
+        public string Content { get; set; } = null!;
+        public List<string> Tecnologies { get; set; } = [];
+
+    }
+}
---
+++ Portfolio/Repository/Repository.cs
@@ -0,0 +1,6 @@
+﻿namespace Portfolio.Repository
+{
+    public class Repository
+    {
+    }
+}
